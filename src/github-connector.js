e29b4697562f0d1cee8404b862c53899f86d4854
Trevor Pierce <trevthewebdev@gmail.com>
2024-11-22 11:48:38 -0600

    Preserve spaces in formatting

--- src/github-connector.js
+++ src/github-connector.js
@@ -99,8 +99,8 @@ class GithubConnector {
 
     return `
       ${HIDDEN_GENERATIVE_TAG}\n
-      <a href="${url}">${key}:&nbsp;${summary}</a>\n\n
-      **Issue Type:** <img src="${issuetypeicon}" /> ${issuetype}\n
+      <a&nbsp;href="${url}">${key}:&nbsp;${summary}</a>\n\n
+      **Issue&nbsp;Type:** <img&nbsp;src="${issuetypeicon}"/>&nbsp;${issuetype}\n
       **Description:**\n
       ${description}\n
       ${HIDDEN_GENERATIVE_TAG}\n\n
